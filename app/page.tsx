2d08ac067d22644759c078c10d0312dd8c57af8c
Joseph <jvp.2703@gmail.com>
2026-04-11 17:13:36 +0100

    add Posts type and fetch posts in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-export default function Home() {
+import type { Posts } from "@/types";
+
+export default async function Home() {
+  const data = await fetch(
+    "https://67bf1a03b2320ee050129b6f.mockapi.io/v1/posts",
+  );
+  const posts = (await data.json()) as Posts;
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
         Hello world from Dockploy!
       </h1>
+      <div className="mt-4">
+        <h2 className="text-2xl font-bold">Posts:</h2>
+        <ul className="">
+          {posts.map((post) => (
+            <li key={post.id} className="mb-2">
+              <h3 className="text-xl font-semibold">{post.title}</h3>
+              <p>{post.body}</p>
+              <span className="text-sm text-gray-500">{post.category}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ types.ts
@@ -0,0 +1,8 @@
+export type Post = {
+  title: string;
+  body: string;
+  category: string;
+  id: number;
+}
+
+export type Posts = Post[];
